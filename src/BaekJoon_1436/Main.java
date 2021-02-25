a568fe30bfa18481f62a02ae490198a75ee1f977
csh <se>
2021-02-25 14:44:20 +0900

    백준 1문제 품(2021.02.25)

--- src/BaekJoon_1436/Main.java
+++ src/BaekJoon_1436/Main.java
@@ -1,7 +1,19 @@
 package BaekJoon_1436;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String num = "666";
+        int cnt = 1;
+        while(n>cnt){
+            num = String.valueOf((Integer.parseInt(num)+1));
+            if(num.contains("666")){
+                cnt++;
+            }
+        }
+        System.out.println(num);
     }
 }
